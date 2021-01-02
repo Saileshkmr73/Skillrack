225e95744efee99b29e70f0398a08d61f751c311
Hemanth Teja Dasari <36339675+hemanthtejadasari@users.noreply.github.com>
2021-01-02 22:47:35 +0530

    Create Shift Right A.py

---
+++ Python Programs/Shift Right A.py
@@ -0,0 +1,57 @@
+"""
+Shift Right A
+The program must accept a character matrix of size RxC containing only A's and B's as the input. The program must remove all occurrences of B and shift the occurrences of A to the right. The empty cells must be replaced with the hyphens. Then the program must print the modified matrix as the output.
+
+Boundary Condition(s):
+2 <= R, C <= 100
+
+Input Format:
+The first line contains R and C separated by a space.
+The next R lines, each contains C alphabets separated by a space.
+
+Output Format:
+The first R lines contain the modified matrix.
+
+Example Input/Output 1:
+Input:
+4 6
+A B A A B B
+A A A A A A
+B B A A A A
+A A A A B A
+
+Output:
+- - - A A A
+A A A A A A
+- - A A A A
+- A A A A A
+
+Explanation:
+After removing all the occurrences of B, the empty cells are replaced with hyphens.
+A - A A - -
+A A A A A A
+- - A A A A
+A A A A - A
+After shifting the occurrences of A to the right, the matrix becomes
+- - - A A A
+A A A A A A
+- - A A A A
+- A A A A A
+
+Example Input/Output 2:
+Input:
+5 5
+B B A B B 
+A B A B A 
+A A B B B 
+B B A A A 
+B B B B A  
+
+Output:
+- - - - A
+- - A A A
+- - - A A
+- - A A A
+- - - - A
+
+"""
