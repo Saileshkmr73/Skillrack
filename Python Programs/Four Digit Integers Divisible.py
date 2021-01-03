bea03689b856fea67fc8fdc9ad817594848a5783
Hemanth Teja Dasari <36339675+hemanthtejadasari@users.noreply.github.com>
2021-01-03 09:14:29 +0530

    Create Four Digit Integers Divisible.py

---
+++ Python Programs/Four Digit Integers Divisible.py
@@ -0,0 +1,38 @@
+"""
+Four Digit Integers Divisible
+The program must accept a string S containing only digits and an integer K as the input. The program must print the number of four-digit integers divisible by K which are formed using the substrings of S.
+Note: The four-digit integers must not have leading zeroes.
+
+Boundary Condition(s):
+4 <= Length of S <= 100
+1 <= K <= 1000
+
+Input Format:
+The first line contains S.
+The second line contains K.
+
+Output Format:
+The first line contains an integer representing the number of four-digit integers divisible by K which are formed using the substrings of S.
+
+Example Input/Output 1:
+Input:
+4287452036
+5
+
+Output:
+2
+
+Explanation:
+Here S = 4287452036 and K = 5.
+The four-digit integers divisible by 5 are 8745 and 4520, which are the substrings of S.
+So their count 2 is printed as the output.
+
+Example Input/Output 2:
+Input:
+53200180
+12
+
+Output:
+0
+
+"""
