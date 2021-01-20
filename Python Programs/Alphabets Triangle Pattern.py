1902fa188549277a1cba0ba811963b386984e030
Hemanth Teja Dasari <36339675+hemanthtejadasari@users.noreply.github.com>
2021-01-20 23:21:37 +0530

    Create Alphabets Triangle Pattern.py

---
+++ Python Programs/Alphabets Triangle Pattern.py
@@ -0,0 +1,44 @@
+"""
+Alphabets Triangle Pattern
+Given an integer N which indicates the position in the alphabet series (upper case) and another integer R which indicates the number of rows to be printed, the program must print R rows with each row containing alphabets equal to the row count.
+
+Boundary Condition(s):
+1 <= N <= 26
+1 <= R <= 26
+
+Example Input/Output 1:
+Input:
+4 5
+
+Output:
+D
+D E
+D E F
+D E F G
+D E F G H
+
+Explanation:
+4 indicates the fourth alphabet which is D.
+5 indicates the number of rows to be printed.
+Row 1: One alphabet starting from D is printed.
+D
+Row 2: Two alphabets starting from D are printed.
+D E
+Row 3: Three alphabets starting from D are printed.
+D E F
+Row 4: Four alphabets starting from D are printed.
+D E F G
+Row 5: Five alphabets starting from D are printed.
+D E F G H
+
+Example Input/Output 2:
+Input:
+25 4
+
+Output:
+Y
+Y Z
+Y Z A
+Y Z A B
+
+"""
