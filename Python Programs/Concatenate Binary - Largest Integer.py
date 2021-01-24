c9f23ea3f7611e8d702304a193f5eede0e8cecb7
Hemanth Teja Dasari <36339675+hemanthtejadasari@users.noreply.github.com>
2021-01-24 22:57:57 +0530

    Create Concatenate Binary - Largest Integer.py

---
+++ Python Programs/Concatenate Binary - Largest Integer.py
@@ -0,0 +1,33 @@
+"""
+Concatenate Binary - Largest Integer
+The program must accept two integers X and Y as the input. The program must print the largest possible integer which is formed by concatenating the binary representations of X and Y as the output.
+
+Boundary Condition(s):
+1 <= X, Y <= 10^4
+
+Input Format:
+The first line contains X and Y separated by a space.
+
+Output Format:
+The first line contains the largest possible integer which is formed by concatenating the binary representations of X and Y.
+
+Example Input/Output 1:
+Input:
+7 10
+
+Output:
+122
+
+Explanation:
+The binary representation of 7 is 111.
+The binary representation of 10 is 1010.
+The largest possible integer formed is 122 and its binary representation is 1111010.
+So 122 is printed as the output.
+
+Example Input/Output 2:
+Input:
+8 5
+
+Output:
+88
+"""
