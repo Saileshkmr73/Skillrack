a2bf4fd037628ca742a094d2e3c4e48a3e38af7b
Hemanth Teja Dasari <36339675+hemanthtejadasari@users.noreply.github.com>
2021-02-02 15:50:40 +0530

    Create Reverse Sum Palindrome.py

---
+++ Python Programs/Reverse Sum Palindrome.py
@@ -0,0 +1,37 @@
+"""
+Reverse Sum Palindrome
+The program must accept an integer N as the input. The program must find the sum of N and its reverse. The program must print the sum if it is a palindromic integer. Else the program must print the next largest palindromic integer of the sum as the output.
+
+Boundary Condition(s):
+2 <= N <= 10^7
+
+Input Format:
+The first line contains N.
+
+Output Format:
+The first line contains an integer based on the given conditions.
+
+Example Input/Output 1:
+Input:
+12
+
+Output:
+33
+
+Explanation:
+Here N = 12.
+The sum of 12 and its reverse 21 is 33 which is a palindromic integer.
+So 33 is printed as the output.
+
+Example Input/Output 2:
+Input:
+8
+
+Output:
+22
+
+Explanation:
+Here N = 8.
+The sum of 8 and its reverse 8 is 16 which is NOT a palindromic integer.
+So the next largest palindromic integer 16 is 22, which is printed as the output.
+"""
